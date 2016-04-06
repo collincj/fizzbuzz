bcd8510121c241d3811e6f7050ddf07bd7793352
Tyler <tzptrans@gmail.com>
2016-04-06 10:15:16 -0400

    Added files via upload

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,32 @@
-exports.count = function(start, end) {
+var count, fizzer, buzzer, fizzbuzzer;
+count = function(start, end) {
 	// initialize the output variable 
-	var output = start;
+	var output = buzzer(fizzer(fizzbuzzer(start)));
 
 	// modify output to contain the desired sequence 
-	for (var counter = start; counter < end; counter += 1) {
+	for (var i = start; i < end; i += 1) {
 		// stuff here gets repeated 
-		output = output + " " + (counter + 1);
+		output = output + " " + buzzer(fizzer(fizzbuzzer(i + 1))); 
 	}
-
 	// return the output, e.g. "1 2 3 4 5"
 	return output;
+};
+
+fizzer = function(num) {
+	return 0 === num % 3 ? "fizz" : num;
+};
+
+buzzer = function(num) {
+	return 0 === num % 5 ? "buzz" : num;
+};
+
+fizzbuzzer = function(num) {
+	return 0 === num % 15 ? "fizzbuzz" : num;
+};
+
+module.exports = {
+	count: count,
+	fizzer: fizzer,
+	buzzer: buzzer,
+	fizzbuzzer: fizzbuzzer
 };
